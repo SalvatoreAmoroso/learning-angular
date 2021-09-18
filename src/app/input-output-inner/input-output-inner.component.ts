2d848e89afa7115a814c187c9a68c752aa5fb433
Salvatore_Titan <amo_salvatore@web.de>
2021-09-18 10:35:26 +0200

    Update Input/Output component

--- src/app/input-output-inner/input-output-inner.component.ts
+++ src/app/input-output-inner/input-output-inner.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-input-output-inner',
   templateUrl: './input-output-inner.component.html',
   styleUrls: ['./input-output-inner.component.scss'],
 })
-export class InputOutputInnerComponent implements OnInit {
+export class InputOutputInnerComponent implements OnInit, OnChanges {
   @Input() passedVar: string = '';
   @Output() changeEvent: EventEmitter<string>;
 
@@ -20,7 +28,22 @@ export class InputOutputInnerComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Input data has changed:\n' + changes);
+  }
+
   inputChanged(text: string) {
+    this.emitChanges(text);
+  }
+
+  reset(): void {
+    const resetStr: string = 'resettet';
+
+    this.passedVar = resetStr;
+    this.emitChanges(resetStr);
+  }
+
+  private emitChanges(text: string) {
     this.changeEvent.emit(text);
     this.passedVarChange.emit(text);
   }
