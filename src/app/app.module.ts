6806623bc944efaad618ef5f028ade28d686cce2
Salvatore Amoroso <amo_salvatore@web.de>
2021-12-15 17:23:08 +0100

    Rename folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,14 +21,14 @@ import { SliderDirectiveDirective } from './directives/slider-directive.directiv
 import { PanelComponent } from './panel/panel.component';
 import { AccordionDirective } from './panel/accordion.directive';
 import { PanelDemoComponent } from './panel-demo/panel-demo.component';
-import { ViewEncapsulationComponent } from './advanced/view-encapsulation/view-encapsulation.component';
-import { TemplateRefComponent } from './advanced/template-ref/template-ref.component';
-import { TemplateRefDemoComponent } from './advanced/template-ref/template-ref-demo/template-ref-demo.component';
-import { ViewContainerRefComponent, CircleComponent, SquareComponent } from './advanced/view-container-ref/view-container-ref.component';
-import { RepeaterDirective } from './advanced/repeater-demo/repeater.directive';
-import { RepeaterDemoComponent } from './advanced/repeater-demo/repeater-demo.component';
-import { ChangeDetectionPerformanceComponent, ContactListComponent, ContactEntryComponent } from './advanced/change-detection-performance/change-detection-performance.component';
-import { ChangeDetectionPerformanceOptimizedComponent, ContactListOptimizedComponent, ContactEntryOptimizedComponent } from './advanced/change-detection-performance-optimized/change-detection-performance-optimized.component';
+import { ViewEncapsulationComponent } from './Advanced/view-encapsulation/view-encapsulation.component';
+import { TemplateRefComponent } from './Advanced/template-ref/template-ref.component';
+import { TemplateRefDemoComponent } from './Advanced/template-ref/template-ref-demo/template-ref-demo.component';
+import { ViewContainerRefComponent, CircleComponent, SquareComponent } from './Advanced/view-container-ref/view-container-ref.component';
+import { RepeaterDirective } from './Advanced/repeater-demo/repeater.directive';
+import { RepeaterDemoComponent } from './Advanced/repeater-demo/repeater-demo.component';
+import { ChangeDetectionPerformanceComponent, ContactListComponent, ContactEntryComponent } from './Advanced/change-detection-performance/change-detection-performance.component';
+import { ChangeDetectionPerformanceOptimizedComponent, ContactListOptimizedComponent, ContactEntryOptimizedComponent } from './Advanced/change-detection-performance-optimized/change-detection-performance-optimized.component';
 import { StandardDirectivesComponent } from './StandardFunctions/standard-directives/standard-directives.component';
 import { PipesComponent } from './StandardFunctions/pipes/pipes.component';
 
