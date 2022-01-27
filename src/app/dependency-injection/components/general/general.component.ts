5d4a5606a18d1f4b41be80fe78b119f3e383f991
Salvatore Amoroso <amo_salvatore@web.de>
2022-01-27 18:44:23 +0100

    Add Treeshakable-Provider example

--- src/app/dependency-injection/components/general/general.component.ts
+++ src/app/dependency-injection/components/general/general.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { DataServiceOne } from '../../services/data-service.service';
+import { DataServiceOne, DataServiceThree } from '../../services/data-service.service';
 
 @Component({
   selector: 'di-general',
@@ -8,9 +8,11 @@ import { DataServiceOne } from '../../services/data-service.service';
 })
 export class GeneralComponent {
 
-  friends: any = [];
+  friends: any[] = [];
+  friendsThree: any[] = [];
 
-  constructor(private dataService: DataServiceOne) { //dataService is injected here. DataServiceOne is defined as a global provider
-    this.friends = dataService.getFriends();
+  constructor(private dataServiceOne: DataServiceOne, private dataServiceThree: DataServiceThree) { //dataService is injected here. DataServiceOne is defined as a global provider
+    this.friends = dataServiceOne.getFriends();
+    this.friendsThree = dataServiceThree.getFriends();
   }
 }
--- src/app/dependency-injection/services/data-service.service.ts
+++ src/app/dependency-injection/services/data-service.service.ts
@@ -13,14 +13,31 @@ export class DataServiceOne extends AbstractDataService {
 
   getFriends() {
     return [
-      { name: "Max", age: this.getRandomInt(65) },
-      { name: "Donald", age: this.getRandomInt(65) }
+      { name: "Max", age: this.getRandomInt(65) }
     ]
   }
 }
 
 @Injectable() //This makes it injectable
 export class DataServiceTwo extends AbstractDataService {
+
+  constructor() { super() }
+
+  getFriends() {
+    return [
+      { name: "Max", age: this.getRandomInt(65) },
+      { name: "Donald", age: this.getRandomInt(65) }
+    ]
+  }
+}
+
+//Treeshakable-Provider
+//Define providers on module level. Treeshakable-Provider DONT replace component-based providers!
+@Injectable({
+  providedIn: "root"
+}) //Since Angular 6 you can specify the provider here. Therefore you dont need to specify the provider in app.module.ts.
+
+export class DataServiceThree extends AbstractDataService {
 
   constructor() { super() }
 
