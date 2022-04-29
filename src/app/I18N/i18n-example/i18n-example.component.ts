342442f658d8c148db461e18beecebbebdb38fe0
Salvatore Amoroso <amo_salvatore@web.de>
2022-04-29 11:11:19 +0200

    Add ICU i18n pipes

--- src/app/I18N/i18n-example/i18n-example.component.ts
+++ src/app/I18N/i18n-example/i18n-example.component.ts
@@ -1,15 +1,46 @@
+import { NgLocalization } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+
+class MinuteCountLocalization extends NgLocalization {
+  getPluralCategory(value: number, locale?: string): string {
+    if (value >= 5 && value < 10) return 'few';
+    if (value >= 10 && value < 20) return 'many';
+    return 'other';
+  }
+}
 
 @Component({
   selector: 'app-i18n-example',
   templateUrl: './i18n-example.component.html',
   styleUrls: ['./i18n-example.component.scss'],
+  providers: [
+    {
+      provide: NgLocalization,
+      useClass: MinuteCountLocalization,
+    },
+  ],
 })
 export class I18nExampleComponent implements OnInit {
   price: number = 3.5;
   minutes: number = 5;
   gender: string = '';
   readonly genders: string[] = ['male', 'female', 'other', 'car'];
+
+  minutesTextMapping: any = {
+    '=0': 'Updated just now',
+    '=1': 'Updated one minute ago',
+    few: 'Updated a few minutes ago', //"few" is provided by MinuteCountLocalization
+    many: 'Many minutes ago', //"many" is provided by MinuteCountLocalization
+    other: 'Updated # minutes ago',
+  };
+
+  genderTextMapping: any = {
+    male: 'a weak male',
+    female: 'an ugly woman',
+    car: 'a what??',
+    other: 'unknown',
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
